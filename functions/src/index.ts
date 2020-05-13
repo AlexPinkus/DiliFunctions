26c231045600d983192a804a1bdef4ff765a8945
alexpinkus <alexpinkus@hotmail.com>
2020-05-13 15:21:22 -0500

    Added functions to cancel, setup and refund intent

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 export { stripeCancelSubscription, stripeCreateSubscription, stripeGetSubscriptions } from './subscriptions';
 export { cleanupUser, stripeCreateCustomer } from './authFunctions';
 export { webhook } from './webhook';
-export { stripeCreateIntent } from './intents';
+export { stripeCreateIntent, stripeCancelIntent, stripeRefundIntent, stripeSetupIntent } from './intents';
 export {
   stripeGetPaymentMethods,
   stripeSavePaymentMethod,
